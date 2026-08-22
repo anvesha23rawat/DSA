379540f387ece7bf3143fa5a4faef52a22eeb61b
anvesha23rawat <151908105+anvesha23rawat@users.noreply.github.com>
2026-08-22 11:19:36 +0530

    Time: 0 ms (100.00%) | Memory: 15.8 MB (14.95%) - LeetSync

---
+++ Arrays/31-next-permutation/next-permutation.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+//solution
+    void nextPermutation(vector<int>& nums) {
+        int gola_index = -1;
+        int n = nums.size();
+        for(int i=n-1;i>0;i--){
+            if(nums[i]>nums[i-1]){
+                gola_index=i-1;
+                break;
+            }
+        }
+        if(gola_index != -1){
+            int swap_index = gola_index;
+            for(int i=n-1;i>=gola_index+1;i--){
+                if(nums[i] > nums[gola_index]){
+                    swap_index = i;
+                    break;
+                }
+            }
+            swap(nums[gola_index],nums[swap_index]);
+        }
+        reverse(nums.begin()+gola_index+1,nums.end());
+        
+    }
+};
