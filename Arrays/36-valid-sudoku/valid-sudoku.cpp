315c10cefefcaf71716c3aabce5f335e576a15c7
anvesha23rawat <151908105+anvesha23rawat@users.noreply.github.com>
2026-09-01 18:22:22 +0530

    Time: 7 ms (27.92%) | Memory: 25.9 MB (8.80%) - LeetSync

--- Arrays/36-valid-sudoku/valid-sudoku.cpp
+++ Arrays/36-valid-sudoku/valid-sudoku.cpp
@@ -1,3 +1,4 @@
+/* brute force approach
 class Solution {
 public:
     bool validBox(vector<vector<char>>& board,int sr,int er,int sc,int ec){
@@ -50,6 +51,31 @@ public:
                 if(!validBox(board,sr,er,sc,ec)){
                     return false;
                 }
+            }
+        }
+        return true;
+    }
+};
+*/
+class Solution {
+public:
+    bool isValidSudoku(vector<vector<char>>& board) {
+        unordered_set<string> st;
+        for(int i=0;i<9;i++){
+            for(int j=0;j<9;j++){
+                if(board[i][j] == '.'){
+                    continue;
+                }
+                string row = string(1,board[i][j]) + "_row_" + to_string(i);
+                string col = string(1,board[i][j]) + "_col_" + to_string(j);
+                string box = string(1,board[i][j]) + "_box_" + to_string(i/3) + "_" + to_string(j/3);
+
+                if(st.find(row)!= st.end() || st.find(col)!= st.end() || st.find(box)!= st.end()){
+                    return false;
+                }
+                st.insert(row);
+                st.insert(col);
+                st.insert(box);
             }
         }
         return true;
