567de0f44edff9059c29bc12cca68e9f51de97e1
anvesha23rawat <151908105+anvesha23rawat@users.noreply.github.com>
2026-09-07 23:46:50 +0530

    Time: 35 ms (34.02%) | Memory: 17.4 MB (77.82%) - LeetSync

--- Arrays/115-distinct-subsequences/distinct-subsequences.cpp
+++ Arrays/115-distinct-subsequences/distinct-subsequences.cpp
@@ -1,21 +1,30 @@
 class Solution {
 public:
-    int numDistinct(string s, string t) {
-        int n = s.size();
-        int m = t.size();
+    typedef unsigned long long ull;
+    ull dp[1001][1001];
 
-        vector<unsigned long long> dp(m + 1, 0);
-
-        dp[0] = 1;
-
-        for (int i = 1; i <= n; i++) {
-            for (int j = min(i, m); j >= 1; j--) {
-                if (s[i - 1] == t[j - 1]) {
-                    dp[j] += dp[j - 1];
-                }
-            }
+    ull solve(string& s,string& t,int m,int n){
+        if(n == 0){
+            return dp[m][n] = 1;//found 1 subsequence
         }
-
-        return (int)dp[m];
+        if(m == 0){
+            return dp[m][n] = 0;
+        }
+        if(dp[m][n] != -1){
+            return dp[m][n];
+        }
+        if(s[m-1] == t[n-1]){
+            return dp[m][n] = solve(s,t,m-1,n-1) + solve(s,t,m-1,n);
+        }
+        else{
+            return dp[m][n] = solve(s,t,m-1,n);
+        }
+        return -1;
+    }
+    int numDistinct(string s, string t) {
+        int m = s.length();
+        int n = t.length();
+        memset(dp,-1,sizeof(dp));
+        return solve(s,t,m,n);
     }
 };
