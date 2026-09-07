7707b5e101d53289dc5b7243abd65e016b52a73b
anvesha23rawat <151908105+anvesha23rawat@users.noreply.github.com>
2026-09-08 00:15:04 +0530

    Time: 12 ms (84.77%) | Memory: 9.1 MB (86.25%) - LeetSync

--- Arrays/115-distinct-subsequences/distinct-subsequences.cpp
+++ Arrays/115-distinct-subsequences/distinct-subsequences.cpp
@@ -28,6 +28,7 @@ public:
     //     return solve(s,t,m,n);
     // }
 
+    /*
     typedef unsigned long long ull;
     int numDistinct(string s, string t) {
         int m = s.length();
@@ -59,5 +60,33 @@ public:
         }
         return dp[m][n];
 
+    }
+    */
+
+
+    //space optimization
+    typedef unsigned long long ull;
+    int numDistinct(string s, string t) {
+        int m = s.length();
+        int n = t.length();
+        vector<ull> curr(n+1,0);
+        vector<ull> prev(n+1,0);
+        //base case 1
+        prev[0] = 1;
+        //base case 2
+        curr[0] = 1;
+        
+        for(int i = 1;i <= m; i++){
+            for(int j = 1; j <= n; j++){
+                if(s[i-1] == t[j-1]){
+                    curr[j] = prev[j-1] + prev[j]; 
+                }
+                else{
+                    curr[j] = prev[j];
+                }
+            }
+            prev = curr;
+        }
+        return prev[n];
     }
 };
