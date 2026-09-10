d1c90e047e8e52d9449d8d65d53d50da5c05f154
anvesha23rawat <151908105+anvesha23rawat@users.noreply.github.com>
2026-09-10 21:34:56 +0530

    Time: 0 ms (100.00%) | Memory: 15.7 MB (64.72%) - LeetSync

--- Arrays/2347-count-nodes-equal-to-average-of-subtree/count-nodes-equal-to-average-of-subtree.cpp
+++ Arrays/2347-count-nodes-equal-to-average-of-subtree/count-nodes-equal-to-average-of-subtree.cpp
@@ -11,31 +11,55 @@
  */
 class Solution {
 public:
+
+    // TC = O(n2)
+    // int result = 0;
+    // int sum = 0;
+    // int findSum(TreeNode* root,int &count){
+    //     if(root == NULL){
+    //         return 0;
+    //     }
+    //     count++;
+    //     int LSum = findSum(root->left,count);
+    //     int RSum = findSum(root->right,count);
+    //     return LSum + RSum + root->val;
+    // }
+    // void solve(TreeNode* root){
+    //     if(root == NULL) {
+    //         return;
+    //     }
+    //     int count = 0;
+    //     sum = findSum(root,count);
+    //     if(root->val == sum/count){
+    //         result++;
+    //     }
+    //     solve(root->left);
+    //     solve(root->right);
+    // }
+    // int averageOfSubtree(TreeNode* root) {
+    //     solve(root);
+    //     return result;
+    // }
+
+    // TC = O(n)
     int result = 0;
-    int sum = 0;
-    int findSum(TreeNode* root,int &count){
+    pair<int,int> solve(TreeNode* root){
         if(root == NULL){
-            return 0;
+            return {0,0};
         }
-        count++;
-        int LSum = findSum(root->left,count);
-        int RSum = findSum(root->right,count);
-        return LSum + RSum + root->val;
-    }
-    void solve(TreeNode* root){
-        if(root == NULL) {
-            return;
-        }
-        int count = 0;
-        sum = findSum(root,count);
-        if(root->val == sum/count){
+        auto p1 = solve(root->left);
+        auto p2 = solve(root->right);
+        int total_sum = p1.first + p2.first + root->val;
+        int total_count = p1.second + p2.second + 1;
+        int avg = total_sum/total_count;
+        if(avg == root->val){
             result++;
         }
-        solve(root->left);
-        solve(root->right);
+        return {total_sum,total_count};
     }
     int averageOfSubtree(TreeNode* root) {
         solve(root);
         return result;
     }
+
 };
